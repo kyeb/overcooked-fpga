169ee52f7497c0443d40441410643875785adb50
jheythere <jarnold360.0@gmail.com>
2020-11-13 16:54:49 -0500

    defining game_logic in/outputs

--- fpga/top_level.sv
+++ fpga/top_level.sv
@@ -12,19 +12,43 @@ module top_level(
    output logic[7:0] an    // Display location 0-7
    );
    
-   assign player_mode = sw[0]; //1 = primary, 0 = secondary
+   //using sw0, 1, 15
+   assign local_player_type = sw[0]; //1 = primary, 0 = secondary
    assign reset = sw[1];  //reset = 1
-   assign carry = sw[15]; //is carrying = 1, put down = 0
+   assign local_carry = sw[15]; //is carrying = 1, put down = 0
    assign led = sw; //check switch is actually on
    
-   logic left, right, up, down, chop;
-   debounce dbchop(.reset_in(reset),.clock_in(clock),.noisy_in(btnc),.clean_out(chop));
-   debounce dbleft(.reset_in(reset),.clock_in(clock),.noisy_in(btnl),.clean_out(left));
-   debounce dbright(.reset_in(reset),.clock_in(clock),.noisy_in(btnr),.clean_out(right));
-   debounce dbup(.reset_in(reset),.clock_in(clock),.noisy_in(btnu),.clean_out(up));
-   debounce dbdown(.reset_in(reset),.clock_in(clock),.noisy_in(btnd),.clean_out(down));
+   //local button inputs
+   logic local_left, local_right, local_up, local_down, local_chop;
+   debounce dbchop(.reset_in(reset),.clock_in(clock),.noisy_in(btnc),.clean_out(local_chop));
+   debounce dbleft(.reset_in(reset),.clock_in(clock),.noisy_in(btnl),.clean_out(local_left));
+   debounce dbright(.reset_in(reset),.clock_in(clock),.noisy_in(btnr),.clean_out(local_right));
+   debounce dbup(.reset_in(reset),.clock_in(clock),.noisy_in(btnu),.clean_out(local_up));
+   debounce dbdown(.reset_in(reset),.clock_in(clock),.noisy_in(btnd),.clean_out(local_down));
    
    assign  dp = 1'b1;  // turn off the period
+   
+   //game logic
+   
+   logic [1:0] num_players;
+   always_comb begin    
+        //if the fpga is primary, designate # of players
+        if (local_player_type) begin
+            num_players = sw[3:2];
+        // else get number from internet
+        end else begin
+            num_players = 2'b01; // ADD LATER
+        end
+   end
+   
+        //inputs: reset, clock, player_type, num_players
+        //inputs for each player: left, right, up, down, chop, carry
+        
+        //output: game state, grid of objects, grid of object times, time left, point total, orders, order times, team_name
+        //output for each player:  player_direction, player_location, player_state
+   
+   //graphics
+   //communication
 
 endmodule
 
