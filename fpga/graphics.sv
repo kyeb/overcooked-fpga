5f6e7c0db609733f03708f3e7e4483358d0c44ea
lacthu <lacthu27@gmail.com>
2020-11-17 17:09:10 -0500

    graphics st

---
+++ fpga/graphics.sv
@@ -0,0 +1,10 @@
+`timescale 1ns / 1ps
+
+module graphics(
+    input reset,
+    input clock,
+    input [1:0] local_player_ID,
+    input [1:0] num_players,
+    input logic [2:0] game_state, // welcome screen or 
+                  
+endmodule
