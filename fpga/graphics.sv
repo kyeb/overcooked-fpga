b924643619f0deb8e16ce393a821155ce07625b4
lacthu <lacthu27@gmail.com>
2020-11-18 20:35:43 -0500

    player test

--- fpga/graphics.sv
+++ fpga/graphics.sv
@@ -40,4 +40,42 @@ module graphics(
         blank_out = blank_in;
     end
 
+    logic [11:0] player_pixel;
+    picture_blob player1 (.pixel_clk_in(clock), .x_in(player_x), .y_in(player_y), .hcount_in(hcount_in), 
+        .vcount_in(vcount_in), .pixel_out(player_pixel));
+
+    assign pixel_out = player_pixel;
+
+endmodule
+
+module picture_blob
+   #(parameter WIDTH = 32,     // default picture width
+               HEIGHT = 32)    // default picture height
+   (input pixel_clk_in,
+    input [10:0] x_in,hcount_in,
+    input [9:0] y_in,vcount_in,
+    output logic [11:0] pixel_out);
+
+   logic [15:0] image_addr;   // num of bits for 256*240 ROM
+   logic [7:0] image_bits, red_mapped, green_mapped, blue_mapped;
+
+   // calculate rom address and read the location
+   assign image_addr = (hcount_in-x_in) + (vcount_in-y_in) * WIDTH;
+   image_rom  rom1(.clka(pixel_clk_in), .addra(image_addr), .douta(image_bits));
+
+   // use color map to create 4 bits R, 4 bits G, 4 bits B
+   // since the image is greyscale, just replicate the red pixels
+   // and not bother with the other two color maps.
+   red_coe rcm (.clka(pixel_clk_in), .addra(image_bits), .douta(red_mapped));
+   green_coe gcm (.clka(pixel_clk_in), .addra(image_bits), .douta(green_mapped));
+   blue_coe bcm (.clka(pixel_clk_in), .addra(image_bits), .douta(blue_mapped));
+   // note the one clock cycle delay in pixel!
+   always_ff @ (posedge pixel_clk_in) begin
+     if ((hcount_in >= x_in && hcount_in < (x_in+WIDTH)) &&
+          (vcount_in >= y_in && vcount_in < (y_in+HEIGHT)))
+        // use MSB 4 bits
+        pixel_out <= {red_mapped[7:4], red_mapped[7:4], red_mapped[7:4]}; // greyscale
+        //pixel_out <= {red_mapped[7:4], 8h'0}; // only red hues
+        else pixel_out <= 0;
+   end
 endmodule
