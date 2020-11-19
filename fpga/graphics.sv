081aadc2088f8bac0d31122e8cfa115b91c49d5e
lacthu <lacthu27@gmail.com>
2020-11-19 12:51:03 -0500

    sprites

--- fpga/graphics.sv
+++ fpga/graphics.sv
@@ -46,6 +46,12 @@ module graphics(
     parameter P_EXT_OFF = 8;
     parameter P_EXT_ON = 9;
 
+    // player directions
+    parameter P_LEFT = 0;
+    parameter P_RIGHT = 1;
+    parameter P_UP = 2;
+    parameter P_DOWN = 3;
+
     // grid object parameters
     parameter G_EMPTY = 0;
     parameter G_ONION_WHOLE = 1;
@@ -61,45 +67,14 @@ module graphics(
 
     // player displays
     logic [11:0] player_pixel;
-    picture_blob player1 (.pixel_clk_in(clock), .x_in(player_x), .y_in(player_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(player_pixel));
+    player_blob player1 (.pixel_clk_in(clock), .x_in(player_x), .y_in(player_y), .hcount_in(hcount), 
+        .vcount_in(vcount), .player_direction(player_direction), .pixel_out(player_pixel));
 
     // grid logic
     logic [2:0] current_grid_x, grid_object_x;
     logic [4:0] current_grid_y, grid_object_y;
     logic [3:0] grid_state;
     pixel_to_grid p2g (.pixel_x(vcount), .pixel_y(vcount), .current_(current_grid_x), .grid_y(current_grid_y));
-
-    // TODO: UPDATE THIS ONCE IMAGES UP
-    picture_blob whole_onion (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(whole_onion));
-
-    picture_blob chopped_onion (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(chopped_onion));
-
-    picture_blob empty_bowl (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(empty_bowl));
-
-    picture_blob full_bowl (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(full_bowl));
-
-    picture_blob empty_pot (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(empty_pot));
-
-    picture_blob raw_pot (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(raw_pot));
-
-    picture_blob cooked_pot (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(cooked_pot));
-
-    picture_blob fire_pot (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(fire_pot));
-
-    picture_blob fire (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(fire));
-
-    picture_blob extinguisher (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
-        .vcount_in(vcount), .pixel_out(extinguisher));
 
     // more grid logic
     always_comb begin
@@ -113,22 +88,22 @@ module graphics(
             end 
         end        
     
-        case (grid_state)
+//        case (grid_state)
         
-            G_EMPTY: object_pixel = 0;
-            G_ONION_WHOLE: object_pixel = whole_onion;
-            G_ONION_CHOPPED: object_pixel = chopped_onion;
-            G_BOWL_EMPTY: object_pixel = empty_bowl;
-            G_BOWL_FULL: object_pixel = full_bowl;
-            G_POT_EMPTY: object_pixel = empty_pot;
-            G_POT_RAW: object_pixel = raw_pot;
-            G_POT_COOKED: object_pixel = cooked_pot;
-            G_POT_FIRE: object_pixel = fire_pot;
-            G_FIRE: object_pixel = fire;
-            G_EXTINGUISHER: object_pixel = extinguisher;
-            default: object_pixel = 0;
+//            G_EMPTY: object_pixel = 0;
+//            G_ONION_WHOLE: object_pixel = whole_onion;
+//            G_ONION_CHOPPED: object_pixel = chopped_onion;
+//            G_BOWL_EMPTY: object_pixel = empty_bowl;
+//            G_BOWL_FULL: object_pixel = full_bowl;
+//            G_POT_EMPTY: object_pixel = empty_pot;
+//            G_POT_RAW: object_pixel = raw_pot;
+//            G_POT_COOKED: object_pixel = cooked_pot;
+//            G_POT_FIRE: object_pixel = fire_pot;
+//            G_FIRE: object_pixel = fire;
+//            G_EXTINGUISHER: object_pixel = extinguisher;
+//            default: object_pixel = 0;
             
-        endcase
+//        endcase
     
         hsync_out = hsync;
         vsync_out = vsync;
@@ -136,38 +111,39 @@ module graphics(
 
     end
 
-    assign pixel_out = player_pixel + object_pixel;
+    assign pixel_out = player_pixel;
+    // object_pixel;
 
 endmodule
 
-module picture_blob
-    #(parameter WIDTH = 32,     // default picture width
-                HEIGHT = 32)    // default picture height
-    (input pixel_clk_in,
-    input [10:0] x_in,hcount_in,
-    input [9:0] y_in,vcount_in,
-    output logic [11:0] pixel_out);
 
-    logic [15:0] image_addr;   // num of bits for 256*240 ROM
-    logic [7:0] image_bits, red_mapped, green_mapped, blue_mapped;
+    // TODO: UPDATE THIS ONCE IMAGES UP
+//    picture_blob whole_onion (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(whole_onion));
 
-    // calculate rom address and read the location
-    assign image_addr = (hcount_in-x_in) + (vcount_in-y_in) * WIDTH;
-    image_rom rom1(.clka(pixel_clk_in), .addra(image_addr), .douta(image_bits));
+//    picture_blob chopped_onion (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(chopped_onion));
 
-    // use color map to create 4 bits R, 4 bits G, 4 bits B
-    // since the image is greyscale, just replicate the red pixels
-    // and not bother with the other two color maps.
-    red_coe rcm (.clka(pixel_clk_in), .addra(image_bits), .douta(red_mapped));
-    green_coe gcm (.clka(pixel_clk_in), .addra(image_bits), .douta(green_mapped));
-    blue_coe bcm (.clka(pixel_clk_in), .addra(image_bits), .douta(blue_mapped));
-    // note the one clock cycle delay in pixel!
-    always_ff @ (posedge pixel_clk_in) begin
-    if ((hcount_in >= x_in && hcount_in < (x_in+WIDTH)) &&
-          (vcount_in >= y_in && vcount_in < (y_in+HEIGHT)))
-        // use MSB 4 bits
-        pixel_out <= {red_mapped[7:4], green_mapped[7:4], green_mapped[7:4]}; // greyscale
-        // pixel_out <= {red_mapped[7:4], 8h'0}; // only red hues
-        else pixel_out <= 0;
-    end
-endmodule
+//    picture_blob empty_bowl (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(empty_bowl));
+
+//    picture_blob full_bowl (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(full_bowl));
+
+//    picture_blob empty_pot (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(empty_pot));
+
+//    picture_blob raw_pot (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(raw_pot));
+
+//    picture_blob cooked_pot (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(cooked_pot));
+
+//    picture_blob fire_pot (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(fire_pot));
+
+//    picture_blob fire (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(fire));
+
+//    picture_blob extinguisher (.pixel_clk_in(clock), .x_in(grid_object_x), .y_in(grid_object_y), .hcount_in(hcount), 
+//        .vcount_in(vcount), .pixel_out(extinguisher));
---
+++ fpga/sprites.sv
@@ -0,0 +1,56 @@
+module player_blob
+    #(parameter WIDTH = 32,     // default picture width
+                HEIGHT = 32)    // default picture height
+    (input pixel_clk_in,
+    input [10:0] x_in,hcount_in,
+    input [9:0] y_in,vcount_in,
+    input [1:0] player_direction,
+    output logic [11:0] pixel_out);
+
+    // player directions
+    localparam P_LEFT = 0;
+    localparam P_RIGHT = 1;
+    localparam P_UP = 2;
+    localparam P_DOWN = 3;
+
+    logic [15:0] image_addr;   // num of bits for 256*240 ROM
+    logic [7:0] image_bits, red_mapped, green_mapped, blue_mapped;
+
+    logic [7:0] up_bits, down_bits, left_bits, right_bits;
+    // calculate rom address and read the location
+    assign image_addr = (hcount_in-x_in) + (vcount_in-y_in) * WIDTH;
+    up_coe up(.clka(pixel_clk_in), .addra(image_addr), .douta(up_bits));
+    down_coe down(.clka(pixel_clk_in), .addra(image_addr), .douta(down_bits));
+    left_coe left(.clka(pixel_clk_in), .addra(image_addr), .douta(left_bits));
+    right_coe right(.clka(pixel_clk_in), .addra(image_addr), .douta(right_bits));
+
+    always_comb begin
+    
+        case (player_direction)
+        
+            P_LEFT: image_bits = left_bits;
+            P_RIGHT: image_bits = right_bits;
+            P_UP: image_bits = up_bits;
+            P_DOWN: image_bits = down_bits;
+        
+        endcase
+    
+    end
+
+
+    // use color map to create 4 bits R, 4 bits G, 4 bits B
+    // since the image is greyscale, just replicate the red pixels
+    // and not bother with the other two color maps.
+    red_coe rcm (.clka(pixel_clk_in), .addra(image_bits), .douta(red_mapped));
+    green_coe gcm (.clka(pixel_clk_in), .addra(image_bits), .douta(green_mapped));
+    blue_coe bcm (.clka(pixel_clk_in), .addra(image_bits), .douta(blue_mapped));
+    // note the one clock cycle delay in pixel!
+    always_ff @ (posedge pixel_clk_in) begin
+    if ((hcount_in >= x_in && hcount_in < (x_in+WIDTH)) &&
+          (vcount_in >= y_in && vcount_in < (y_in+HEIGHT)))
+        // use MSB 4 bits
+        pixel_out <= {red_mapped[7:4], green_mapped[7:4], green_mapped[7:4]}; // greyscale
+        // pixel_out <= {red_mapped[7:4], 8h'0}; // only red hues
+        else pixel_out <= 0;
+    end
+endmodule
